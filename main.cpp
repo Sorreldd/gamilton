ec1c911f3235dce9dd42a9a83fca9da71953b9f4
Sorreldd <artyom.sorrel@gmail.com>
2013-02-25 11:39:34 +0300

    bug fixed; added comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+/*
+    Комаров Артём
+    2743
+*/
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,12 +11,18 @@ int n, m, v, u, w, a[50][50], mask, ans;
 
 int main() {
     cin >> n >> m;
+    if(n == 1) {
+        cout << 0;
+        // вырожденный случай
+        return 0;
+    }
     for(int i = 0; i < m; i++) {
         cin >> u >> v >> w;
         --u; --v;
         a[u][v] = a[v][u] = w;
     }
 ////////////////////////////////////////////////////////
+    // База динамики, заполнение подмножеств бесконечностями, количество масок 2^n
     mask = 1 << n;
     int dp[50][mask];
     for(int i = 0; i < n; ++i)
@@ -20,6 +30,7 @@ int main() {
             dp[i][j] = inf;
     dp[0][1] = 0;
 ////////////////////////////////////////////////////////
+    // Динамика по подмножествам
     for(int i = 0; i < n; i++)
         for(int j = 1; j < mask; j++)
             if(dp[i][j] != inf)
@@ -27,10 +38,12 @@ int main() {
                     if(a[i][k] != 0 && (j | (1 << k) != j))
                         dp[k][j | (1 << k)] = min(dp[k][j | (1 << k)], dp[i][j] + a[i][k]);
 ///////////////////////////////////////////////////////
+    // Нахождение ответа и его вывод
     ans = inf;
     for(int i = 0; i < n; i++)
         if(a[i][0] != 0)
             ans = min(ans, dp[i][mask - 1] + a[i][0]);
-    cout << ans;
+    if(ans != inf)cout << ans;
+    else "Cycle not found";
     return 0;
 }
